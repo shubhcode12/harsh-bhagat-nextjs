d08badfe9df3f313f784c58f65fee07a1b90fc0c
shubhcode <shubhcode.dev@gmail.com>
2024-06-26 13:45:59 +0530

    fix footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,33 +1,43 @@
 import React from "react";
+import { FaTelegram } from "react-icons/fa6";
+import { IoLogoInstagram } from "react-icons/io5";
+import { RiYoutubeFill } from "react-icons/ri";
+import { FaSquareInstagram } from "react-icons/fa6";
 
 function Footer() {
   return (
     <footer className="bg-[#1c1c1c] text-white ">
       <div className="px-6 pt-32 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24  lg:px-16">
         <div className="mx-auto px-4 text-center">
-          <div className="flex flex-col lg:flex-row lg:justify-between align-center pb-20">
-            <div>
-              <h4 className="font-bold">Find me on YouTube</h4>
-              <ul className="mt-4">
-                <li>Channel 1</li>
-                <li>Channel 1</li>
-                <li>Channel 1</li>
-              </ul>
+          <div className="flex flex-col lg:flex-row items-center lg:justify-between align-center pb-20">
+            <div className="flex justify-center space-x-4 py-8">
+              <img
+                className="w-32 object-cover invert"
+                src="harsh-bhagat-logo.png"
+                alt="harsh bahagat"
+              />
             </div>
+
             <div className="sm:mt-4 md:mt-4 lg:mt-0 xl:mt-0 mt-4">
-              <h4 className="font-bold">Drop me an E-mail</h4>
-              <form className="mt-4">
-                <input
-                  type="email"
-                  className="px-4 py-2 rounded-l-lg"
-                  placeholder="Your email"
-                />
-                <button
-                  type="submit"
-                  className="bg-green-500 px-4 py-2 rounded-r-lg">
-                  Send
-                </button>
-              </form>
+              <h4 className="text-white font-bold">Find me on</h4>
+
+              <div className="flex justify-center space-x-4">
+                <a
+                  href="https://www.instagram.com/meharshbhagat"
+                  className="text-green-500">
+                  <FaSquareInstagram className="w-8 h-8" />
+                </a>
+                <a
+                  href="https://www.youtube.com/@meharshbhagat"
+                  className="text-green-500">
+                  <RiYoutubeFill className="w-8 h-8" />
+                </a>
+                <a
+                  href="https://telegram.openinapp.co/iueeh"
+                  className="text-green-500">
+                  <FaTelegram className="w-8 h-8" />
+                </a>
+              </div>
             </div>
           </div>
           <div className="mt-8 p-5 text-xl">
