6fe21fe8af0beda9e84f5273cf935a60990175cc
shubhcode <shubhcode.dev@gmail.com>
2024-06-27 15:55:52 +0530

    add hyperlink to header logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,17 +16,22 @@ function Header() {
     { title: "Awards", link: "https://harshbhagat.com/#awards" },
     { title: "About", link: "https://harshbhagat.com/#about" },
     { title: "Support", link: "/support" },
-    { title: "Private Community", link: "https://harshbhagat.com/#privateCommunity" },
+    {
+      title: "Private Community",
+      link: "https://harshbhagat.com/#privateCommunity",
+    },
     { title: "Contact", link: "https://harshbhagat.com/#contact" },
   ];
 
   return (
     <header className="fixed top-0 left-0 right-0 z-50 flex justify-between bg-transparent backdrop-blur-lg border-solid border-b-2 border-[#c1ffe7] items-center p-5 shadow-md bg-white">
-      <img
-        className="w-24 object-cover"
-        src="harsh-bhagat-logo.png"
-        alt="harsh bahagat"
-      />
+      <a href="/">
+        <img
+          className="w-24 object-cover"
+          src="harsh-bhagat-logo.png"
+          alt="harsh bahagat"
+        />
+      </a>
       <nav className="text-xl hidden md:flex">
         {menuItems.map((item, i) => (
           <Link href={item.link} className="px-3">
